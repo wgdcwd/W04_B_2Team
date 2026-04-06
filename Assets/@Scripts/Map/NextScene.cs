f524c874e28cbb0644c2dad39fe5c7c7f1a0647d
wgdcwd <86083157+wgdcwd@users.noreply.github.com>
2026-04-06 19:28:44 +0900

    스테이지 시작,끝 줌인,아웃판정의 pixel perfect disable 적용

--- Assets/@Scripts/Map/NextScene.cs
+++ Assets/@Scripts/Map/NextScene.cs
@@ -2,47 +2,48 @@ using DG.Tweening;
 using System;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.U2D;
 
 public class NextScene : MonoBehaviour
 {
     [SerializeField] private string _sceneName;
-
     [SerializeField] bool _isBossStage = false;
     [SerializeField] bool _isLastStage = false;
-
     [SerializeField] CinemachineCamera _vcam;
-
     private SceneFlowManager _sceneFlowManager;
     private CheckpointManager _checkpointManager;
     private GameStateManager _gameStateManager;
-
     private void Awake()
     {
         ManagerRegistry.TryGet(out _sceneFlowManager);
         ManagerRegistry.TryGet(out _checkpointManager);
         ManagerRegistry.TryGet(out _gameStateManager);
     }
-
     public void NextStage()
     {
         _vcam = GetComponentInChildren<CinemachineCamera>();
         _vcam.Priority = 100;
+
+        PixelPerfectCamera pixelPerfect = Camera.main.GetComponent<PixelPerfectCamera>();
+        if (pixelPerfect != null) pixelPerfect.enabled = false;
+
         DOTween.To(
             () => _vcam.Lens.OrthographicSize,
             x => _vcam.Lens.OrthographicSize = x,
             0.1f,
             2f
         ).SetEase(Ease.OutCubic)
-        .OnComplete(() =>  // 줌인 끝난 후 씬 전환
+        .OnComplete(() =>
         {
+            if (pixelPerfect != null) pixelPerfect.enabled = true;
+
             if (_sceneFlowManager == null)
                 return;
-
             if (_gameStateManager == null || _gameStateManager.CurrentState != GameState.Respawning)
             {
                 _checkpointManager?.ClearCheckpoint();
             }
-
             if (_isBossStage)
             {
                 GameManager.Instance.GameClear();
@@ -51,12 +52,9 @@ public class NextScene : MonoBehaviour
             _sceneFlowManager.LoadStage();
         });
     }
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
-
         if (player != null)
         {
             NextStage();
